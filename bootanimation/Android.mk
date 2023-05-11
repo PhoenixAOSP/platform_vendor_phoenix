6f917e96fbefaca5b73cd9f88957df61cd6ea1fd
Lunarixus <Nathan@lunarixus.dev>
2023-05-11 04:44:59 +0000

    bootanimation: Add new meme boot animation
    * This is really just a placeholder while we find a designer.
    
    Signed-off-by: Lunarixus <Nathan@lunarixus.dev>

--- bootanimation/Android.mk
+++ bootanimation/Android.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2022 Raphielscape LLC.
+# Copyright (C) 2023 PhoenixAOSP..
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
